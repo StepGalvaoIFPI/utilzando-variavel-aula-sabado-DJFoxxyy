830b554fa833d076930c808e72769129583f8137
DJFoxxyy <pedrorato24@gmail.com>
2025-04-15 21:25:02 -0300

    Código inteiro
    Terminado

--- q1.py
+++ q1.py
@@ -1,1 +1,57 @@
-
+#Questôes 1 a 3
+name = "José Pedro"
+idade = 18
+curso = "Computação"
+print(name)
+print(idade)
+print(curso)
+#Questão 4
+a = 3
+b = 5
+resultado = a*b
+print(resultado)
+#Questão 5
+num1 = 10
+num2 = 5
+num3 = 8
+media = (num1+num2+num3)/3
+print(media)
+#Questão 6
+salario = 1050
+imposto = 1050*0.20
+print(imposto)
+#Questão 7
+base = 10
+altura = 5
+area = base*altura/2
+print(area)
+#Questão 8
+nota1 = 8
+nota2 = 7
+nota3 = 6
+media = (nota1+nota2+nota3)/3
+print(media)
+#Questão 9
+Salario = 1500
+Imposto = 1500*0.15
+print(Imposto)
+#Questão 10
+SalaBruto = 5000
+INSS = 5000*0.15
+IR = 5000*0.10
+Recebido = 5000-5000*0.25
+print(SalaBruto)
+print(INSS)
+print(IR)
+print(Recebido)
+#Questão 11
+Raio = 10
+Area = 3.1415*10**2
+print(Area)
+#Questão 12
+A = 8
+B = 10
+Media = (A*3.5+7.5*B)/11
+print(A)
+print(B)
+print(Media)
